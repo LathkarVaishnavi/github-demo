6fde65b9ce1fa923aa8685454544e6c4d4d50177
LathkarVaishnavi <79468068+LathkarVaishnavi@users.noreply.github.com>
2021-11-07 20:14:22 +0530

    new variable added

--- add.java
+++ add.java
@@ -2,7 +2,8 @@ class Add
 {
   public static void main(string args[])
   {
-    int a=10,b=10;
-    System.out.print("addition = "+a+b);
+    int a=10,b=10,c;
+    c=a+b;
+    System.out.print("addition = "+c);
   }
 }
